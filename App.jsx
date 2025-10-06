a8b418534bdb8a75416aa40e6884a18a1d952f51
pb-06 <kkandomail@gmail.com>
2025-10-06 07:42:25 +0200

    Create project

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import Spinner from './Spinner'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const timeout = async function(milliSecs) {
+    return new Promise((resolve, reject)=>{
+      setTimeout( ()=>resolve('timeout completed'), milliSecs)
+    })
+  }
+
+  const handleTimeoutClick = () => {
+
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleTimeoutClick}>
+          Timeout now
+        </button>
+        
+        <Spinner />
+      </div>
+    </>
+  )
+}
+
+export default App
